b05551983f8358bb662858cacb3d403e9815bda3
tony20090521 <34048897+tony20090521@users.noreply.github.com>
2018-01-07 15:10:33 +0800

    Update MainActivity.java

--- app/src/main/java/com/afmobi/testlaunchermode/MainActivity.java
+++ app/src/main/java/com/afmobi/testlaunchermode/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
 
     private String testStr = "test";
 
+    private String testStr2 = "test2";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
